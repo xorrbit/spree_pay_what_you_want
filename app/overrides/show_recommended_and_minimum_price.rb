39ebd5826aa80d45ec70d102fed11a33e57f2d83
Andrew Orr <aorr@iders.ca>
2013-01-04 21:14:07 -0600

    Added minimum price to product view and removed actual price

---
+++ app/overrides/show_recommended_and_minimum_price.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(:virtual_path => "spree/products/_cart_form",
+		     :replace => "#product-price",
+	             :text => '<div id="product-price">
+                                 <h6 class="product-section-title"><%= t(:recommended_price) %></h6>
+		                 <div><span class="price selling" itemprop="price"><%= @product.recommended_price %></span></div>
+			         <h6 class="product-section-title"><%= t(:minimum_price) %></h6>
+		                 <div><span class="price selling" itemprop="price"><%= @product.minimum_price %></span></div>
+		              </div>',
+		     :name => "show_recommended_and_minimum_price")
--- app/overrides/show_recommended_price.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/products/_cart_form",
-		     :insert_before => "[data-hook='product_price']",
-	             :text => "<h1>Recommended Price: <%= @product.recommended_price %><br/>Minimum Price: <%= @product.minimum_price %></h1>",
-		     :name => "show_recommended_price")
