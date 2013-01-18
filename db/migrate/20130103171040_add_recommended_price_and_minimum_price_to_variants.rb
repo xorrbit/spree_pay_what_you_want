9b2c79bcbd3dc7b62d8e6c56bddac12154f00d0e
Andrew Orr <aorr@iders.ca>
2013-01-17 22:29:49 -0600

    Added initialization to recommende_price and minimum_price in the migrations

--- db/migrate/20130103171040_add_recommended_price_and_minimum_price_to_variants.rb
+++ db/migrate/20130103171040_add_recommended_price_and_minimum_price_to_variants.rb
@@ -1,7 +1,22 @@
+# This migration comes from spree_pay_what_you_want (originally 20130103171040)
 class AddRecommendedPriceAndMinimumPriceToVariants < ActiveRecord::Migration
   def change
     # 8.2 decimal
     add_column :spree_variants, :recommended_price, :decimal, :precision => 8, :scale => 2, :default => 0
     add_column :spree_variants, :minimum_price, :decimal, :precision => 8, :scale => 2, :default => 0
+
+    Spree::Variant.reset_column_information
+
+    say_with_time 'Setting recommend_price and minimum_price to variant price' do
+      Spree::Variant.all.each do |v|
+        if v.price
+          v.update_attributes!(:recommended_price => v.price, :minimum_price => v.price)
+        elsif v.product && v.product.master.price
+          v.update_attributes!(:recommended_price => v.product.master.price, :minimum_price => v.product.master.price)
+        else
+          raise 'No price found to copy over to recommended_price and minimum_price'
+        end
+      end
+    end
   end
 end
--- db/migrate/20130111035513_add_recommended_price_and_minimum_price_to_line_items.rb
+++ db/migrate/20130111035513_add_recommended_price_and_minimum_price_to_line_items.rb
@@ -3,5 +3,19 @@ class AddRecommendedPriceAndMinimumPriceToLineItems < ActiveRecord::Migration
     # 8.2 decimal
     add_column :spree_line_items, :recommended_price, :decimal, :precision => 8, :scale => 2, :default => 0
     add_column :spree_line_items, :minimum_price, :decimal, :precision => 8, :scale => 2, :default => 0
+
+    Spree::LineItem.reset_column_information
+
+    say_with_time 'Setting recommend_price and minimum_price to line item price' do
+      Spree::LineItem.all.each do |l|
+        if l.price
+          l.update_attributes!(:recommended_price => l.price, :minimum_price => l.price)
+        elsif l.variant
+          l.update_attributes!(:recommended_price => l.variant.price, :minimum_price => l.variant.price)
+        else
+          raise 'No price found to copy over to recommended_price and minimum_price'
+        end
+      end
+    end
   end
 end
