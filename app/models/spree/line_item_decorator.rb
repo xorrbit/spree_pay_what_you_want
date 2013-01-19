7210945bfd86b6671197571585f5ed886cc860cc
Benjamin Bergman <ben@benbergman.ca>
2013-01-18 20:33:24 -0600

    Add some validation

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,3 +1,12 @@
 Spree::LineItem.class_eval do
   attr_accessible :recommended_price, :minimum_price
+  validates :minimum_price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :recommended_price, numericality: {
+    greater_than_or_equal_to: :minimum_price,
+    message: 'must be greater than or equal to minimum price.'
+  }
+  validates :price, numericality: {
+    greater_than_or_equal_to: :minimum_price,
+    message: 'must be greater than or equal to minimum price.'
+  }
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,4 +3,9 @@
 Spree::Product.class_eval do
   delegate_belongs_to :master, :recommended_price, :minimum_price
   attr_accessible :recommended_price, :minimum_price
+  validates :minimum_price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :recommended_price, numericality: {
+    greater_than_or_equal_to: :minimum_price,
+    message: 'must be greater than or equal to minimum price.'
+  }
 end
