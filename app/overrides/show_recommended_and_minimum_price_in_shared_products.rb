8253c85b6b0c47aa993c03a893242302c271344d
Benjamin Bergman <ben@benbergman.ca>
2013-02-03 18:49:06 -0600

    Fix price ordering on product listings

--- app/overrides/show_recommended_and_minimum_price_in_shared_products.rb
+++ app/overrides/show_recommended_and_minimum_price_in_shared_products.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :replace => ".price",
-                     :text => '<span class="price selling" itemprop="price"><%= number_to_currency(product.recommended_price) %> to <%= number_to_currency(product.minimum_price) %></span>',
+                     :text => '<span class="price selling" itemprop="price"><%= number_to_currency(product.minimum_price) %> to <%= number_to_currency(product.recommended_price) %></span>',
                      :name => "show_recommended_and_minimum_price_in_shared_products")
